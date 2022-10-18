52cada5e30171e802ae0c2a0f72e3e46998270fd
ibirsan <ibirsan@threatconnect.com>
2022-10-18 15:58:46 +0300

    add test

---
+++ variables/test.js
@@ -0,0 +1,11 @@
+// console.log('this is my first test');
+/*
+let firstName = 'Ioana';
+console.log(firstName);
+
+const lastName = 'birsan';
+
+lastName = 'X';
+console.log(lastName);
+*/
+console.log(typeof alert);
